af83a8a5e926285c6eb7ef26c98e62b9944addf8
Elena Ilyukhina <elena.ilyukhina@hotmail.com>
2018-01-07 21:19:59 +0100

    user-agent

--- remoteupload.php
+++ remoteupload.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('user_agent','astro-snail');
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 $data = json_decode(file_get_contents('php://input'), true);
 print_r($data);
@@ -12,6 +16,9 @@ echo $clone_url, PHP_EOL;
 $contents_url = strstr($data["repository"]["contents_url"],"{", true);
 $contents_url = substr($contents_url, 0, strlen($contents_url)-1);
 echo $contents_url, PHP_EOL;
+
+$contents = json_decode(file_get_contents($contents_url), true);
+print_r($contents);
 
 exec('php copy-files.php "' . addslashes($contents_url) . '"', $output);
 print_r($output);
